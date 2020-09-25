82552324090f4898cad232f6039c684f0a0f0a1f
ahmedbashir-repo <ahmedbashir.w@gmail.com>
2020-09-25 14:24:52 +0500

    JS Alert, Variables, Mathematical Ops

---
+++ script.js
@@ -0,0 +1,21 @@
+var firstName = "Usman";
+var lastName = "Ali";
+var fullName = firstName + " " + lastName;
+
+//Mathematical Operators
+var num1 = 10.6;
+var num2 = 20;
+var sum = num1 + num2;
+var diff = num1 - num2;
+var product = num1 * num2;
+var div = num1 / num2;
+var rem = num1 % num2;
+console.log("Sum = " + sum);
+console.log("Difference = " + diff);
+console.log("Product = " + product);
+console.log("Division = " + div);
+console.log("Remainder = " + rem);
+
+// var username = prompt("Enter Username: ");
+// console.log(username);
+
